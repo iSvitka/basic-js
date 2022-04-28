818ac145c5e2704f2eb9c25864529878801f70a4
iSvitka <svitka.ilya@gmail.com>
2022-04-28 15:02:32 +0300

    feat: complete count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(cats) {
+  let counter = 0;
+  
+  if(!cats){
+    return 0;
+  }
+
+  for(let i = 0; i < cats.length; i++){
+    for(let j = 0; j < cats[i].length; j++){
+      if(cats[i][j] === '^^'){
+        counter++;
+      }
+    }
+  }
+
+  return counter;
 }
 
 module.exports = {
