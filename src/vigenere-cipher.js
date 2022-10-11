076bf2a6e4289491714818ac789e2029025937ab
iSvitka <svitka.ilya@gmail.com>
2022-10-11 11:19:37 +0300

    feat: complete vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,77 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direction = true) {
+    this.direction = direction;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if(!(message && key)) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    message = message.toUpperCase().split('').map(char => {
+      if(char.toUpperCase() !== char.toLowerCase()){
+        return char = char.charCodeAt(0) - 65;
+      }
+      return char;
+    });
+    key = key.toUpperCase().split('').map(char => {
+      if(char.toUpperCase() !== char.toLowerCase()){
+        return char = char.charCodeAt(0) - 65;
+      }
+      return char;
+    });
+
+    let encryptedMessage  = [];
+    let currentChar = 0;
+
+    for(let i = 0; i < message.length; i++){
+      if(typeof(message[i]) === 'number'){
+        encryptedMessage.push(String.fromCharCode((message[i] + key[currentChar % key.length]) % 26 + 65));
+        currentChar++;
+      } else {
+        encryptedMessage.push(message[i]);
+      }
+    }
+
+    return this.direction ? encryptedMessage.join('') : encryptedMessage.reverse().join('');
+  }
+  
+  decrypt(message, key) {
+    if(!(message && key)){
+      throw new Error('Incorrect arguments!');
+    }
+
+    message = message.toUpperCase().split('').map(char => {
+      if(char.toUpperCase() !== char.toLowerCase()){
+        return char = char.charCodeAt(0) - 65;
+      }
+      return char;
+    });
+    key = key.toUpperCase().split('').map(char => {
+      if(char.toUpperCase() !== char.toLowerCase()){
+        return char = char.charCodeAt(0) - 65;
+      }
+      return char;
+    });
+
+    let decryptedMessage  = [];
+    let currentChar = 0;
+
+    for(let i = 0; i < message.length; i++){
+      if(typeof(message[i]) === 'number'){
+        decryptedMessage.push(String.fromCharCode((message[i] - key[currentChar % key.length] + 26) % 26 + 65));
+        currentChar++;
+      } else {
+        decryptedMessage.push(message[i]);
+      }
+    }
+
+    return this.direction ? decryptedMessage.join('') : decryptedMessage.reverse().join('');
   }
 }
 
